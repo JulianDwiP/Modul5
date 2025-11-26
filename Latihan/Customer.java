e2dba8a3b95bf9899d9c431d9c12de795d239f4d
JulianDwiP <juliandwi74@gmail.com>
2025-11-26 14:29:38 +0700

    Perbaikan Pronounceable Names

---
+++ Latihan/Customer.java
@@ -0,0 +1,10 @@
+package Latihan;
+
+import java.sql.Date;
+
+class Customer {
+    private Date creationTimestamp;
+    private Date modificationTimestamp;
+    private final String recordId = "102";
+    /* ... */
+}
