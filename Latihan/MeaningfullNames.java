1412f59df24343cb616a8c0fa58567f1b256d8d6
JulianDwiP <54734720+JulianDwiP@users.noreply.github.com>
2025-11-26 14:13:53 +0700

    Add files via upload

---
+++ Latihan/MeaningfullNames.java
@@ -0,0 +1,14 @@
+package Latihan;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MeaningfullNames {
+    public List<int[]> getThem() {
+    List<int[]> list1 = new ArrayList<int[]>();
+    for (int[] x : theList)
+        if (x[0] == 4)
+            list1.add(x);
+    return list1;
+}
+}
